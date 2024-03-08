70e0d7e94801cd9180d787d4e92b9f6394ce09f8
Pavel Lazar <pavel@alazara.com>
2024-03-08 14:43:56 -0500

    chore: updates

--- src/main.js
+++ src/main.js
@@ -1,38 +1,145 @@
-import { sleep } from "k6";
 import http from "k6/http";
-import {
-  randomString,
-} from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import { check } from 'k6';
 
-export const options = {
-  duration: "10m",
-  vus: 10,
-  rps: 20,
-  thresholds: {
-    http_req_duration: ["p(95)<1000"], // 95% of requests should complete below 1s (.5 for get, .5 for set)
+// Define the set of keys
+let keys = Array.from({length: 1000}, (_, i) => `key${i}`);
+
+// Define the payloads
+let smallPayload = 'x'.repeat(1024); // 1K
+let mediumPayload = 'x'.repeat(10240); // 10K
+let largePayload = 'x'.repeat(3 * 1024 * 1024); // 3MB
+
+// Define the scenarios
+export let options = {
+  scenarios: {
+    // smallSet: {
+    //   executor: 'constant-arrival-rate',
+    //   rate: 1000,
+    //   timeUnit: '1s',
+    //   duration: '10m',
+    //   preAllocatedVUs: 100,
+    //   maxVUs: 200,
+    //   exec: 'smallSet',
+    // },
+    // smallGet: {
+    //   executor: 'constant-arrival-rate',
+    //   rate: 1000,
+    //   timeUnit: '1s',
+    //   duration: '10m',
+    //   preAllocatedVUs: 100,
+    //   maxVUs: 200,
+    //   exec: 'smallGet',
+    // },
+    // mediumSet: {
+    //   executor: 'constant-arrival-rate',
+    //   rate: 1000,
+    //   timeUnit: '1s',
+    //   duration: '10m',
+    //   preAllocatedVUs: 100,
+    //   maxVUs: 200,
+    //   exec: 'mediumSet',
+    // },
+    // mediumGet: {
+    //   executor: 'constant-arrival-rate',
+    //   rate: 1000,
+    //   timeUnit: '1s',
+    //   duration: '10m',
+    //   preAllocatedVUs: 100,
+    //   maxVUs: 200,
+    //   exec: 'mediumGet',
+    // },
+    // largeSet: {
+    //   executor: 'constant-arrival-rate',
+    //   rate: 5,
+    //   timeUnit: '1s',
+    //   duration: '10m',
+    //   preAllocatedVUs: 10,
+    //   maxVUs: 10,
+    //   exec: 'largeSet',
+    // },
+    largeGet: {
+      executor: 'constant-arrival-rate',
+      rate: 5,
+      timeUnit: '1s',
+      duration: '10m',
+      preAllocatedVUs: 10,
+      maxVUs: 10,
+      exec: 'largeGet',
+    },
   },
 };
 
-export default function () {
-  const key = randomString(10);
-  const data = randomString(5 * 1024 * 1024);
-  
-  http.post(
+export function smallSet() {
+  let key = keys[Math.floor(Math.random() * keys.length)];
+  let res = http.post(
     "https://mk0wcy94wf.execute-api.us-east-1.amazonaws.com/prod",
     JSON.stringify({
       operation: "set",
       key,
-      data,
+      data: smallPayload,
     })
   );
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
 
-  http.post(
+export function smallGet() {
+  let key = keys[Math.floor(Math.random() * keys.length)];
+  let res =   http.post(
     "https://mk0wcy94wf.execute-api.us-east-1.amazonaws.com/prod",
     JSON.stringify({
       operation: "get",
       key,
     })
   );
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
 
-  sleep(1);
+export function mediumSet() {
+  let key = keys[Math.floor(Math.random() * keys.length)];
+  let res = http.post(
+    "https://mk0wcy94wf.execute-api.us-east-1.amazonaws.com/prod",
+    JSON.stringify({
+      operation: "set",
+      key,
+      data: mediumPayload,
+    })
+  );
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
+
+export function mediumGet() {
+  let key = keys[Math.floor(Math.random() * keys.length)];
+  let res =   http.post(
+    "https://mk0wcy94wf.execute-api.us-east-1.amazonaws.com/prod",
+    JSON.stringify({
+      operation: "get",
+      key,
+    })
+  );
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
+
+export function largeSet() {
+  let key = keys[Math.floor(Math.random() * keys.length)];
+  let res = http.post(
+    "https://mk0wcy94wf.execute-api.us-east-1.amazonaws.com/prod",
+    JSON.stringify({
+      operation: "set",
+      key,
+      data: largePayload,
+    })
+  );
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
+
+export function largeGet() {
+  let key = keys[Math.floor(Math.random() * keys.length)];
+  let res =   http.post(
+    "https://mk0wcy94wf.execute-api.us-east-1.amazonaws.com/prod",
+    JSON.stringify({
+      operation: "get",
+      key,
+    })
+  );
+  check(res, { 'status was 200': (r) => r.status == 200 });
 }
